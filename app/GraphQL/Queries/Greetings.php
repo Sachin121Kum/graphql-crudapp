c3a83678b269e60f1ace7681377e759552196fcc
Sachin Kumar <sachin.kumar21@globallogic.com>
2024-01-28 12:25:14 +0530

    query all user data and single user data query changes

---
+++ app/GraphQL/Queries/Greetings.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+final class Greetings
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        // return "Hello World";
+
+
+        
+    }
+}
